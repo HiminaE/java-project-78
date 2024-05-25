64e84a062edaea8067cc272db1ddbdbd541eb5d3
HiminaE <himinaev@yandex.ru>
2024-05-25 19:41:13 +0300

    add NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,4 +1,22 @@
 package hexlet.code.schemas;
 
-public class NumberSchema {
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public final class NumberSchema extends BaseSchema<Integer> {
+    public NumberSchema required() {
+        Predicate<Integer> required = Objects::nonNull;
+        mapOfPredicates.put("required", required);
+        return this;
+    }
+    public NumberSchema positive() {
+        Predicate<Integer> pos = num -> num == null || num > 0;
+        mapOfPredicates.put("positive", pos);
+        return this;
+    }
+    public NumberSchema range(int min, int max) {
+        Predicate<Integer> range = num -> num != null && num >= min && num <= max;
+        mapOfPredicates.put("range", range);
+        return this;
+    }
 }
