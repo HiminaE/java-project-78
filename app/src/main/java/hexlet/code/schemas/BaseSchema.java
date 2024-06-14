b5baef66351fdacd839af040fe1d0a2bc0f62306
HiminaE <himinaev@yandex.ru>
2024-06-14 08:15:23 +0300

    correct NumberTest

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,7 +4,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-
 public abstract class BaseSchema<T> {
     protected Map<String, Predicate<T>> mapOfPredicates = new LinkedHashMap<>();
     public final boolean isValid(T t) {
--- app/src/test/java/hexlet/code/NumberSchemaTests.java
+++ app/src/test/java/hexlet/code/NumberSchemaTests.java
@@ -1,36 +1,37 @@
 package hexlet.code;
 
 import hexlet.code.schemas.NumberSchema;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberSchemaTests {
-    private NumberSchema n;
-    @BeforeEach
-    void beforeEach() {
-        Validator val = new Validator();
-        n = val.number();
-    }
     @Test
     public void testRequired() {
+        Validator val = new Validator();
+        NumberSchema n = val.number();
         assertTrue(n.required().isValid(5));
         assertFalse(n.required().isValid(null));
     }
     @Test
     public void testPositive() {
+        Validator val = new Validator();
+        NumberSchema n = val.number();
         assertTrue(n.positive().isValid(5));
         assertFalse(n.positive().isValid(-1));
     }
     @Test
     public void testRrange() {
+        Validator val = new Validator();
+        NumberSchema n = val.number();
         assertTrue(n.range(1, 9).isValid(5));
         assertFalse(n.range(1, 3).isValid(5));
     }
     @Test
     public void testAccumulation() {
+        Validator val = new Validator();
+        NumberSchema n = val.number();
         n.required();
         assertFalse(n.isValid(null));
         assertTrue(n.isValid(7));
@@ -42,6 +43,8 @@ public class NumberSchemaTests {
     }
     @Test
     public void testAll() {
+        Validator val = new Validator();
+        NumberSchema n = val.number();
         assertTrue(n.required().positive().range(2, 9).isValid(7));
         assertFalse(n.required().positive().range(5, 10).isValid(2));
         assertFalse(n.required().positive().range(5, 10).isValid(-1));
