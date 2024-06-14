51a1098bbac43f6986c0d83fdddb69df53d6dbb0
HiminaE <himinaev@yandex.ru>
2024-06-14 08:20:37 +0300

    correct StringTest

--- app/src/test/java/hexlet/code/StringSchemaTests.java
+++ app/src/test/java/hexlet/code/StringSchemaTests.java
@@ -1,38 +1,39 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringSchemaTests {
-    private StringSchema s;
-    @BeforeEach
-    void beforeEach() {
-        Validator val = new Validator();
-        s = val.string();
-    }
     @Test
     public void testRequired() {
+        Validator val = new Validator();
+        StringSchema s = val.string();
         assertTrue(s.required().isValid("string"));
         assertFalse(s.required().isValid(""));
         assertFalse(s.required().isValid(null));
     }
     @Test
     public void testMinLength() {
+        Validator val = new Validator();
+        StringSchema s = val.string();
         String str = "hello";
         assertTrue(s.minLength(4).isValid(str));
         assertFalse(s.minLength(6).isValid(str));
     }
     @Test
     public void testContain() {
+        Validator val = new Validator();
+        StringSchema s = val.string();
         String str = "hello";
         assertTrue(s.contains("hel").isValid(str));
         assertFalse(s.contains("or").isValid(str));
     }
     @Test
     public void testAccumulation() {
+        Validator val = new Validator();
+        StringSchema s = val.string();
         String str = "hello";
         assertTrue(s.isValid(null));
         s.required();
@@ -46,6 +47,8 @@ public class StringSchemaTests {
     }
     @Test
     public void testAll() {
+        Validator val = new Validator();
+        StringSchema s = val.string();
         String str = "hello";
         assertTrue(s.required().minLength(4).contains("hel").isValid(str));
         assertFalse(s.required().minLength(10).contains("ex").isValid(str));
